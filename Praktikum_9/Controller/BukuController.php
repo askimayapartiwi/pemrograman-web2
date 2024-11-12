5882ed6a00923013e7a5ad07682184195fe2283b
askimayapartiwi <askimayapartiwi@gmail.com>
2024-11-12 10:50:07 +0700

    Menambahkan Tugas 9

---
+++ Praktikum_9/Controller/BukuController.php
@@ -0,0 +1,67 @@
+                                                                                                                                                                                <?php
+
+require_once "Model/DaftarBuku.php";
+
+class BukuController{
+    public function jalankan(){
+        //mengakses model
+        $data = new DaftarBuku();
+
+        // memberi data model ke view dan menampilkan view
+        include "View/BukuView.php";
+    }
+
+    public function simpan(){
+        $buku = new Buku($_POST['judul'], $_POST['pengarang'], $_POST['penerbit'], $_POST['tahun']);
+
+        $daftar_buku = new DaftarBuku();
+        $daftar_buku->simpan($buku);
+
+        header('Location: http://localhost/index.php');
+        exit;
+    }
+
+    public function hapus(){
+        $id = $_POST['id_hapus'];
+
+        $daftar_buku = new DaftarBuku();
+        $daftar_buku->hapus($id);
+
+        header('Location: http://localhost/index.php');
+        exit;
+    }
+
+    public function edit(){
+        // index.php/edit?id=5
+
+        // Menangkap Nilai Query String id dari View
+        $id = $_GET['id'];
+
+        // Membuat Objek Model Daftar Buku
+        $daftar_buku = new DaftarBuku();
+
+        // Mengambil dan Membuat Objek Model Buku berdasarkan id buku dari objek daftar_buku;
+        $buku = $daftar_buku->getBukuById($id);
+
+        // Jika buku ada atau ketemu
+        if($buku){
+            // Tampilkan View Edit Buku
+            include_once "view/EditBukuView.php";
+        }else{
+            header("Location: http://localhost/index.php");
+        }
+    }
+
+    public function update(){
+        $buku = new Buku($_POST['judul'], $_POST['pengarang'], $_POST['penerbit'], $_POST['tahun']);
+        $buku->setId($_POST['id']);
+
+        //var_dump($buku);
+        //die();
+
+        $daftar_buku = new DaftarBuku();
+        $daftar_buku->update($buku);
+
+        header('Location: http://localhost/index.php');
+    }
+}
